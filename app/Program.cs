7146e933a55f83464d8afe277669385ad327f6e7
Raz Carolino <russelle.carolino@infor.com>
2024-11-07 01:00:21 +0800

    defect/VCMS65 - Project clean-up

--- app/Program.cs
+++ app/Program.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using app.view;
-using app.View;
 using app.view.Administration;
-using app.View.Patient;
 
 namespace app
 {
