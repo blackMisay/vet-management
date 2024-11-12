de2cec5b4b60cfd58a081d5bbdae0147e0d56108
reinskye <lyrapearlc@gmail.com>
2024-11-12 11:02:14 +0800

    enhance/VCMS67

---
+++ app/Core/Repository/TransactionRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.core.repository
+{
+    internal class TransactionRepository
+    {
+        public int Id { get; set; }
+        public int ClientID { get; set; }
+        public int ProductID { get; set; }
+        public int ServiceID { get; set; }
+        public int SoapID { get; set; }
+        public string Qty { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date {  get; set; }
+    }
+}
